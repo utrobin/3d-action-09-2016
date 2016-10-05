b98c5cd75be5a4b003093b214e8dc122e44478bc
madsunrise <rudnev.vanya@gmail.com>
2016-10-05 22:05:44 +0300

    Test interface

---
+++ src/test/java/ru/javajava/main/test.java
@@ -0,0 +1,23 @@
+package ru.javajava.main;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by ivan on 05.10.16.
+ */
+public class test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+    @After
+    public void tearDown() {
+        System.out.println("finished");
+    }
+    @Test
+    public void testSomething() {
+
+    }
+}
