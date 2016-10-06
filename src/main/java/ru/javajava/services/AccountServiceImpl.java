609dba6e6ed4d785eae4b0d571ff9f6436eb56a3
madsunrise <rudnev.vanya@gmail.com>
2016-10-06 16:28:02 +0300

    Fixed names

--- src/main/java/ru/javajava/services/AccountServiceImpl.java
+++ src/main/java/ru/javajava/services/AccountServiceImpl.java
@@ -3,28 +3,29 @@ package ru.javajava.services;
 import org.springframework.stereotype.Service;
 import ru.javajava.model.UserProfile;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Stream;
 
 @Service
 public class AccountServiceImpl implements AccountService {
     private final Map<String, UserProfile> loginToProfile = new ConcurrentHashMap<>();
-    private final Map<Long, UserProfile> IdToProfile = new ConcurrentHashMap<>();
+    private final Map<Long, UserProfile> idToProfile = new ConcurrentHashMap<>();
 
+    @Override
     public UserProfile addUser(String login, String password, String email) {
         final UserProfile userProfile = new UserProfile(login, email, password);
         loginToProfile.put(login, userProfile);
-        IdToProfile.put(userProfile.getId(), userProfile);
+        idToProfile.put(userProfile.getId(), userProfile);
         return userProfile;
     }
 
+    @Override
     public UserProfile getUserByLogin(String login) {
         return loginToProfile.get(login);
     }
 
-    public UserProfile getUserById(Long Id) {
-        return IdToProfile.get(Id);
+    @Override
+    public UserProfile getUserById(Long id) {
+        return idToProfile.get(id);
     }
 }
