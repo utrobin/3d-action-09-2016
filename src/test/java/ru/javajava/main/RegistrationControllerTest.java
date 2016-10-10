27e12234e8d07553bb4dc70041bbcd783a0fe2f5
madsunrise <rudnev.vanya@gmail.com>
2016-10-10 22:45:25 +0300

    Removed useless Test and added new blank test

---
+++ src/test/java/ru/javajava/main/RegistrationControllerTest.java
@@ -0,0 +1,26 @@
+package ru.javajava.main;
+
+import org.junit.*;
+import org.junit.Test;
+
+
+/**
+ * Created by ivan on 10.10.16.
+ */
+public class RegistrationControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void signup() throws Exception {
+
+    }
+
+    @Test
+    public void login() throws Exception {
+
+    }
+
+}
