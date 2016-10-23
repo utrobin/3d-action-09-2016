eecee3a7464f37cbcc21c16b1b97585cb33c595a
madsunrise <rudnev.vanya@gmail.com>
2016-10-23 19:23:47 +0300

    Created DB AccountService

---
+++ src/main/java/ru/javajava/services/AccountServiceDatabaseImpl.java
@@ -0,0 +1,23 @@
+package ru.javajava.services;
+
+import ru.javajava.model.UserProfile;
+
+/**
+ * Created by Администратор on 23.10.2016.
+ */
+public class AccountServiceDatabaseImpl implements AccountService {
+    @Override
+    public UserProfile addUser(String login, String password, String email) {
+        return null;
+    }
+
+    @Override
+    public UserProfile getUserByLogin(String login) {
+        return null;
+    }
+
+    @Override
+    public UserProfile getUserById(Long id) {
+        return null;
+    }
+}
