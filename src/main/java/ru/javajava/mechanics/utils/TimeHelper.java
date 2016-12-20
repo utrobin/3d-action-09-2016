b8c1e25015345c3aa978d4af1da0159251a188c0
madsunrise <rudnev.vanya@gmail.com>
2016-12-20 21:16:44 +0300

    Excluded e.printStackTrace()

--- src/main/java/ru/javajava/mechanics/utils/TimeHelper.java
+++ src/main/java/ru/javajava/mechanics/utils/TimeHelper.java
@@ -1,22 +1,20 @@
 package ru.javajava.mechanics.utils;
 
-public class TimeHelper {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public final class TimeHelper {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TimeHelper.class);
+	private TimeHelper() { }
 	public static void sleep(long period){
 		if (period <= 0) {
 			return;
 		}
 		try{
 			Thread.sleep(period);
-		} catch (InterruptedException e) {				
-			e.printStackTrace();
-		}
-	}
-	
-	public static void sleep(){
-		try{
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {				
-			e.printStackTrace();
+		} catch (InterruptedException e) {
+			LOGGER.debug("We were interrupted!");
 		}
 	}
 }
